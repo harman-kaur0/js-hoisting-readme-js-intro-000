71c6009eb7985ef075a99e897e96901b10f25732
balanced-checkpoint-e039 <harmandeepk628@gmail.com>
2021-03-03 02:23:53 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -11,9 +11,9 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+  var thisIsCrazy
 
-  var thisIsCrazy = function (){
+  function thisIsCrazy(){
     console.log("hey!!!")
   }
 }
